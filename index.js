a7d2db8ed7191ba96a83763dc923845444c3fd22
Ihar <iharkrutoi@gmail.com>
2024-03-17 22:10:01 +0300

    js: navigate through menu nav

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const buttonMenuNav = document.querySelectorAll('.nav-menu-button');
 
 buttonMenuNav.forEach((e) => {
-    // console.log(e.dataset.menuNav);
-
+    
     function clickButtonActive() {
         (() => {
             buttonMenuNav.forEach((e) => {
                 e.classList.remove('active');
             })
         })();
-
+        
         e.classList.add('active');
+
+        const block = document.querySelectorAll('.block');
+
+        block.forEach((e) => {
+            e.setAttribute('style', 'display: none');
+        })
+        
+        const blockActive = document.querySelector(`.${e.dataset.menuNav}`);
+        blockActive.setAttribute('style', 'display: grid');
     }
 
     e.addEventListener('click', clickButtonActive);
