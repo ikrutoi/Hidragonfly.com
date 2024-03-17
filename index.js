12bcb64e4fe0ca790cf4179f1172c8bc4a335076
Ihar <iharkrutoi@gmail.com>
2024-03-17 18:46:09 +0300

    begin JS

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const buttonMenuNav = document.querySelectorAll('.nav-menu-button');
 
 buttonMenuNav.forEach((e) => {
-    e.style = 'color: green';
-})
+    // console.log(e.dataset.menuNav);
+
+    function clickButtonActive() {
+        (() => {
+            buttonMenuNav.forEach((e) => {
+                e.classList.remove('active');
+            })
+        })();
+
+        e.classList.add('active');
+    }
+
+    e.addEventListener('click', clickButtonActive);
+});
