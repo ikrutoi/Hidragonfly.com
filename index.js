38f573cabd3498ae6c23a540855a5a1f67f1cbdf
Ihar <iharkrutoi@gmail.com>
2024-03-18 13:58:37 +0300

    add card-letter animation cursor

--- index.js
+++ index.js
@@ -8,18 +8,41 @@ buttonMenuNav.forEach((e) => {
                 e.classList.remove('active');
             })
         })();
-        
+              
         e.classList.add('active');
-
+        
+        function showBlockTimer() {
+            blockActive.setAttribute('style', 'display: grid');
+        }
+        
         const block = document.querySelectorAll('.block');
-
+        
         block.forEach((e) => {
             e.setAttribute('style', 'display: none');
         })
         
         const blockActive = document.querySelector(`.${e.dataset.menuNav}`);
-        blockActive.setAttribute('style', 'display: grid');
+        
+        setTimeout(showBlockTimer, 100);
+        
+        const menuNavBlock = document.querySelectorAll('.menu-nav-block');
+        
+        menuNavBlock.forEach((e) => {
+            e.setAttribute('style', 'display: none');
+        })
+
+        const menuNavBlockActive = document.querySelector(`.${e.dataset.menuNavBlock}`);
+        menuNavBlockActive.setAttribute('style', 'display: block');
+
+        // const cardLetterCursorBlock = document.querySelector('.card-letter-cursor-block');
+        
+        // setInterval(() => {
+        //     cardLetterCursorBlock.setAttribute('style', 'opacity: 0.2');
+        // }, 500);
+
     }
 
     e.addEventListener('click', clickButtonActive);
+    
 });
+
