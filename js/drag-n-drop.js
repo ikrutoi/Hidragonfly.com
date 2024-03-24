baec776e46a2a8a5acc8de64aac01cb2552a146b
Ihar <iharkrutoi@gmail.com>
2024-03-24 13:41:38 +0300

    dran-n-drop replace mouse events -> pointer events

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -29,20 +29,20 @@ export function dragNDrop() {
             return false;
         };
         
-        el.onmousedown = function(ev) {
-            const retreatBlockImgTop = document.querySelector('.new-img-rubber').getBoundingClientRect().top;
-            const retreatBlockImgLeft = document.querySelector('.new-img-rubber').getBoundingClientRect().left;
-            const blockNewImgTop = document.querySelector('.new-img-rubber').getBoundingClientRect().top;
+        // el.onmousedown = function(ev) {
+        el.onpointerdown = function(ev) {
+            el.setPointerCapture(ev.pointerId);
+            
+            const rubberBlockImgTop = document.querySelector('.new-img-rubber').getBoundingClientRect().top;
+            const rubberBlockImgLeft = document.querySelector('.new-img-rubber').getBoundingClientRect().left;
             
             moveAt(ev.clientX, ev.clientY);
             
             function moveAt(pageX, pageY) {
-                const elTop = pageY - retreatBlockImgTop - el.getBoundingClientRect().height / 2 + 'px';
-                const elLeft = pageX  - retreatBlockImgLeft - el.getBoundingClientRect().width / 2 + 'px';
-
+                const elTop = pageY - rubberBlockImgTop - el.getBoundingClientRect().height / 2 + 'px';
+                const elLeft = pageX  - rubberBlockImgLeft - el.getBoundingClientRect().width / 2 + 'px';
                 el.style.top = elTop;
                 el.style.left = elLeft;
-
                 el.className.split(' ').forEach((nameClass) => {
                     if(
                         nameClass == 'crop-circle-1' || 
@@ -52,14 +52,12 @@ export function dragNDrop() {
                             keepProportions(nameClass);
                         } 
                 })
-                
+            
                 function keepProportions(nameClass) {
-
                     switch (nameClass) {
                         case 'crop-circle-1': {
                             const c1Left = circle1start.getBoundingClientRect().left;
                             const delta1 = (circle1.getBoundingClientRect().left - c1Left) / 1.42;
-                            
                             circle1.style.top = delta1 +'px';
                             circle2.style.top = circle1.style.top;
                             circle4.style.left = circle1.style.left;
@@ -68,7 +66,6 @@ export function dragNDrop() {
                         case 'crop-circle-2': {
                             const c2Left = circle2start.getBoundingClientRect().left;
                             const delta2 = (c2Left - circle2.getBoundingClientRect().left) / 1.42;
-                            
                             circle2.style.top = delta2 +'px';
                             circle1.style.top = circle2.style.top;
                             circle3.style.left = circle2.style.left;
@@ -77,8 +74,7 @@ export function dragNDrop() {
                         case 'crop-circle-3': {    
                             const c3Left = circle3start.getBoundingClientRect().left;
                             const delta3 = (c3Left - circle3.getBoundingClientRect().left) / 1.42;
-                            
-                            circle3.style.top = circle3start.getBoundingClientRect().top - blockNewImgTop - delta3 + 'px';
+                            circle3.style.top = circle3start.getBoundingClientRect().top - rubberBlockImgTop - delta3 + 'px';
                             circle4.style.top = circle3.style.top;
                             circle2.style.left = circle3.style.left;
                         }
@@ -86,32 +82,33 @@ export function dragNDrop() {
                         case 'crop-circle-4': {
                             const c4Left = circle4start.getBoundingClientRect().left;
                             const delta4 = (circle4.getBoundingClientRect().left - c4Left) / 1.42;
-                            
-                            circle4.style.top = circle4start.getBoundingClientRect().top - blockNewImgTop - delta4 + 'px';
+                            circle4.style.top = circle4start.getBoundingClientRect().top - rubberBlockImgTop - delta4 + 'px';
                             circle3.style.top = circle4.style.top;
                             circle1.style.left = circle4.style.left;
                         }
                         break;       
                     }
                 }
-
-                const elWidth = circle2.getBoundingClientRect().left - circle1.getBoundingClientRect().left;
-                const elHeight = circle3.getBoundingClientRect().top - circle2.getBoundingClientRect().top;
-                const styleNewImgRubber = `
-                    width: ${elWidth}px;
-                    height: ${elHeight}px;`;
-                newArea.setAttribute('style', `${styleNewImgRubber}`);
+            // const elWidth = circle2.getBoundingClientRect().left - circle1.getBoundingClientRect().left;
+            // const elHeight = circle3.getBoundingClientRect().top - circle2.getBoundingClientRect().top;
+            // const styleNewImgRubber = `
+            //     width: ${elWidth}px;
+            //     height: ${elHeight}px;`;
+            // newArea.setAttribute('style', `${styleNewImgRubber}`);
             } 
-        
-            function onMouseMove(ev) {
+    
+        // function onMouseMove(ev) {
+            function onPointerMove(ev) {
                 moveAt(ev.clientX, ev.clientY)
             }
-
-            document.addEventListener('mousemove', onMouseMove);
-
-            el.onmouseup = function() {
-                document.removeEventListener('mousemove', onMouseMove);
-                el.onmouseup = null;
+            // document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('pointermove', onPointerMove);
+            // el.onmouseup = function() {
+            el.onpointerup = function() {
+                // document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('pointermove', onPointerMove);
+                // el.onmouseup = null;
+                el.onpointerup = null;
             }
         };
     });
