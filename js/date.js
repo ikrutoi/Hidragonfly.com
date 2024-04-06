bc6c4eb4c996a2317743ed3b58c28a144d2e6a6e
Ihar <iharkrutoi@gmail.com>
2024-04-06 11:36:04 +0300

    correct work filling date in month

--- js/date.js
+++ js/date.js
@@ -4,7 +4,8 @@ export function createCalendar() {
     let valueDate = new Date();
     let year = valueDate.getFullYear();
     let numberMonth = valueDate.getMonth();
-    let day = valueDate.getDate();
+    let date = valueDate.getDate();
+    let day = valueDate.getDay();
     const nameMonth = [
         'January', 
         'February', 
@@ -44,37 +45,85 @@ export function createCalendar() {
     newElemHTML(areaDateDays, 'beforeend', '<table class="date-table"></table>');
 
     const blockTable = document.querySelector('.date-table');
+    
+    // newElemHTML(blockTable, 'beforeend', '<thead class="date-table-header"></thead>');
+    newElemHTML(blockTable, 'beforeend', '<tbody class="date-table-body"></tbody>');
+    
+    // const tableHeader = document.querySelector('.date-table-header');
+    const tableBody = document.querySelector('.date-table-body');
 
-    newElemHTML(blockTable, 'beforeend', '<tr class="table-row-header"></tr>');
+    newElemHTML(tableBody, 'beforeend', '<tr class="date-table-header-row"></tr>');
 
-    const rowTableHeader = document.querySelector('.table-row-header');
-    const nameDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', ];
+    const tableHeaderRow = document.querySelector('.date-table-header-row');
+    const nameDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    function addRow(day) {
+    function getFirstDay(year, month) {
+        let firstDay = new Date(year, month, 1);
+
+        return firstDay.getDay();
+    }
+
+    let numberFirstDay = getFirstDay(year, numberMonth);
+
+    function addRow() {
         for (let i = 0; i < 7; i++) {
-            newElemHTML(rowTableHeader, 'beforeend', `<th>${day[i]}</th>`);
+            newElemHTML(tableHeaderRow, 'beforeend', `<th>${nameDays[i]}</th>`);
         }
 
         function getQuantityDaysOfMonth(year, month) {
-            let date = new Date(year, month + 1, 0);
+            let dateLastDay = new Date(year, month + 1, 0);
     
-            return date.getDate();
+            return dateLastDay.getDate();
         }
           
         let quantityDaysOfMonth = getQuantityDaysOfMonth(year, numberMonth);
         let quanityRows = Math.ceil(quantityDaysOfMonth / 7);
+        let dayCounter = 1;
 
         for (let i = 1; i <= quanityRows; i++) {
-            newElemHTML(blockTable, 'beforeend', `<tr class="table-row-${i}"></tr>`);
+            newElemHTML(tableBody, 'beforeend', `<tr class="table-row-${i}"></tr>`);
 
             const tableRow = document.querySelector(`.table-row-${i}`);
 
-            for (let i = 0; i < 7; i++) {
-                newElemHTML(tableRow, 'beforeend', '<td></td>');
+            if (i == 1) {   
+                for (let i = 0; i < 7; i++) {
+                    if (i <= numberFirstDay) {
+                        newElemHTML(tableRow, 'beforeend', `<td></td>`);
+                    } else 
+                    newElemHTML(tableRow, 'beforeend', `<td>${dayCounter++}</td>`);
+                }
+            } else {
+                for (let i = 0; i < 7; i++) {
+                    if (dayCounter <= quantityDaysOfMonth) {
+                        newElemHTML(tableRow, 'beforeend', `<td>${dayCounter++}</td>`);
+                    } else 
+                    newElemHTML(tableRow, 'beforeend', `<td></td>`);
+                }
             }
         }
+
+        // function entryEqualWidthHeight() {
+        //     const blockTableWidth = blockTable.getBoundingClientRect();
+        //     const tableColumnWidth = blockTableWidth.width / 7;
+        //     const tableRowHeight = blockTableWidth.height / (quanityRows + 1);
+
+        //     const tableColumn = document.querySelectorAll('th');
+            
+        //     tableColumn.forEach(el => {
+        //         el.setAttribute('style', `width: ${tableColumnWidth}px;`);
+        //     })
+
+
+        //     console.log(tableColumnWidth);
+        //     console.log(tableRowHeight);
+        // }
+
+
+        // setTimeout(entryEqualWidthHeight, 300);
+
+
     }
 
-    addRow(nameDays);
+    addRow();
 }
 
