6a7474524d7580befe82e8ce50a97cd964417dc9
Ihar <iharkrutoi@gmail.com>
2024-06-22 22:43:47 +0300

    corrected code

--- js/card-photo.js
+++ js/card-photo.js
@@ -5,23 +5,31 @@ import { newElemHTML } from "./new-element.js";
 export function formationCardPhoto() {   
     const elemCardphoto = document.querySelector('.cardphoto');
     const elemCardphotoInput = document.querySelector('.cardphoto-input');
+    const elemCardphotoImage = document.querySelector('.cardphoto-img');
     
     function addActive() {
         this.classList.add('active');
-        setTimeout(() => {
-                newElemHTML(elemCardphoto, 'beforeend', '<div class="cardphoto-newimg"></div>');  
-            }, 1000);
+        // setTimeout(() => {
+        //         newElemHTML(elemCardphoto, 'beforeend', '<div class="cardphoto-newimg"></div>');  
+        //     }, 1000);
         }
         
     function checkImgSelection(event) {
         console.log('this.value: ', this.value);
+        elemCardphotoImage.classList.add('active');
         const newImageFile = document.querySelector('.cardphoto-input').files[0];
-        fetch('/upload', {method: 'POST', body: new FormData().append('image', newImageFile)})
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-        console.log('*++*', event.target.files.length);
-        console.log('**', event.target);
+        // const newImageFile2 = event.files[0];
+        const imageURL = URL.createObjectURL(newImageFile);
+        elemCardphotoImage.src = imageURL;
+        event.value = null;
+        elemCardphotoImage.onload = () => URL.revokeObjectURL(imageURL);
+
+        // fetch('/upload', {method: 'POST', body: new FormData().append('image', newImageFile)})
+        // .then(response => response.json())
+        // .then(data => console.log(data))
+        // .catch(error => console.error(error));
+        // console.log('*++*', event.target.files.length);
+        // console.log('**', event.target);
     }
     
     elemCardphoto.addEventListener('mouseenter', addClassHover);
@@ -29,11 +37,4 @@ export function formationCardPhoto() {
     elemCardphoto.addEventListener('pointerdown', addActive);
     
     elemCardphotoInput.addEventListener('change', checkImgSelection);
-
-    elemCardphotoInput.addEventListener('change', (event) => {
-        console.log('-*-*-*-*--*');
-        if (!event.target.files.length) {
-          console.log('Выбор файла отменён. Или что-то другое произошло?');
-        }
-    });
 }
