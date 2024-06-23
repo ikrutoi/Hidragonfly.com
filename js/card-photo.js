7505cd3021152fc709b7cf4f10263db971de000b
Ihar <iharkrutoi@gmail.com>
2024-06-23 17:17:29 +0300

    corrected code

--- js/card-photo.js
+++ js/card-photo.js
@@ -1,14 +1,19 @@
+import { startPressActivation } from "./start-press-activation.js";
 import { addClassHover } from "./start-press-activation.js";
 import { delClassHover } from "./start-press-activation.js";
 import { newElemHTML } from "./new-element.js";
 
 export function formationCardPhoto() {   
     const elemCardphoto = document.querySelector('.cardphoto');
+    const elemCardphotoImageNav = document.querySelector('.cardphoto-img-nav');
+    const elemCardphotoImageNavButton = document.querySelectorAll('.cardphoto-img-nav-button');
     const elemCardphotoInput = document.querySelector('.cardphoto-input');
+    const elemCardphotoImageStart = document.querySelector('.cardphoto-img-start');
     const elemCardphotoImage = document.querySelector('.cardphoto-img');
     
     function addActive() {
         this.classList.add('active');
+
         // setTimeout(() => {
         //         newElemHTML(elemCardphoto, 'beforeend', '<div class="cardphoto-newimg"></div>');  
         //     }, 1000);
@@ -16,9 +21,9 @@ export function formationCardPhoto() {
         
     function checkImgSelection(event) {
         console.log('this.value: ', this.value);
+        elemCardphotoImageStart.classList.add('deactivation');
         elemCardphotoImage.classList.add('active');
         const newImageFile = document.querySelector('.cardphoto-input').files[0];
-        // const newImageFile2 = event.files[0];
         const imageURL = URL.createObjectURL(newImageFile);
         elemCardphotoImage.src = imageURL;
         event.value = null;
@@ -31,10 +36,51 @@ export function formationCardPhoto() {
         // console.log('*++*', event.target.files.length);
         // console.log('**', event.target);
     }
+
+    function addClassActive () {
+        elemCardphotoImageNav.classList.add('active');
+        elemCardphotoImageNavButton.forEach(el => el.classList.add('wait-start'));
+        setTimeout(() => elemCardphotoImageNavButton.forEach(el => el.classList.add('wait')), 150);
+    }
+    
+    function delClassActive () {
+        elemCardphotoImageNavButton.forEach(el => el.classList.remove('wait'));
+        setTimeout(() => {
+            elemCardphotoImageNav.classList.remove('active');
+            elemCardphotoImageNavButton.forEach(el => el.classList.remove('wait-start'));
+        }, 150);
+    }
+
+    function validationImageNavButton() {
+        switch(this.dataset.cardphotoNav) {
+            case 'add':
+                elemCardphotoInput.addEventListener('change', checkImgSelection);
+                break; 
+            case 'cut':
+                console.log('cut');
+                break; 
+            case 'cntr':
+                console.log('cntr');
+                break; 
+            case 'torn':
+                console.log('torn');
+                break; 
+            case 'del':
+                console.log('del');
+                break; 
+        }
+    }
     
     elemCardphoto.addEventListener('mouseenter', addClassHover);
+    elemCardphoto.addEventListener('mouseenter', addClassActive);
     elemCardphoto.addEventListener('mouseleave', delClassHover);
-    elemCardphoto.addEventListener('pointerdown', addActive);
+    elemCardphoto.addEventListener('mouseleave', delClassActive);
+    elemCardphoto.addEventListener('pointerdown', addActive); 
     
-    elemCardphotoInput.addEventListener('change', checkImgSelection);
+    elemCardphotoImageNavButton.forEach(el => {
+        el.addEventListener('mouseenter', addClassHover);
+        el.addEventListener('mouseleave', delClassHover);
+        el.addEventListener('pointerdown', () => startPressActivation(el));
+        el.addEventListener('pointerdown', validationImageNavButton);
+    })
 }
